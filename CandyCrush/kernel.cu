0061c4d28329216e5f84d7a3a9eb01dcf3addadc
pabloggarc <pablogg.ext.88.22@gmail.com>
2023-03-25 18:23:55 +0100

    Meter aleatorios chapuza

--- CandyCrush/kernel.cu
+++ CandyCrush/kernel.cu
@@ -4,12 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <curand_kernel.h>
 
-int N = 1; //filas
-int M = 1;  //columnas
+//Variables generales del juego
+
+int N;
+int M;
 int vidas = 5; 
+int modo;
 int dificultad; 
-int modo; 
+__constant__ int nuevos_caramelos_facil[4] = { 1, 2, 3, 4 };
+__constant__ int nuevos_caramelos_dificil[6] = { 1, 2, 3, 4, 5, 6 };
+
+//Funciones auxiliares (DEVICE)
+
+/*
+    x: vector de enteros
+    n: longitud del vector
+    y: elemento a encontrar
+
+    Salida: determina si y pertenece a x
+
+*/
 
 __host__ __device__ bool pertenece(int* x, int n, int y) {
     bool p = false;
@@ -18,6 +34,20 @@ __host__ __device__ bool pertenece(int* x, int n, int y) {
     }
     return p;
 }
+
+/*
+    tablero: tablero del juego
+    inicio: casilla en desde la que se empieza a buscar
+    fin: casilla a la que se quiere llegar
+    visitados: casillas por las que se ha intentado ir
+    x: longitud de visitados
+    camino: casillas que forman parte del camino
+    y: longitud de camino
+    N y M: dimensiones del tablero
+
+    Salida: camino que lleva de inicio a fin (mediante el puntero a camino)
+
+*/
 
 __device__ void buscar_camino(char* tablero, int inicio, int fin, int* visitados, int* x, int* camino, int* y, int N, int M) {
     if (inicio != fin) {
@@ -57,7 +87,60 @@ __device__ void buscar_camino(char* tablero, int inicio, int fin, int* visitados
     }
 }
 
-__device__ void recolocar_tablero(char* tablero, int N, int M) {
+/*
+    tablero: tablero del juego
+    N y M: dimensiones del tablero
+    fila y columna: del elemento tocado
+
+    Salida: tablero con las posiciones a borrar sustituidas por 'X' (mediante el puntero a tablero)
+
+*/
+
+__global__ void encontrar_caminos(char* tablero, int N, int M, int fila, int columna) {
+    int selec = fila * M + columna;
+    int id = threadIdx.y * N + threadIdx.x;
+
+    //Funcion que busque camino
+    int* camino = (int*)malloc(N * M * sizeof(int));
+    int* visitados = (int*)malloc(N * M * sizeof(int));
+    int x = 1;
+    int y = 1;
+
+    for (int i = 1; i < N * M; ++i) {
+        camino[i] = -1;
+        visitados[i] = -1;
+    }
+
+    camino[0] = id;
+    visitados[0] = id;
+
+    if (tablero[selec] == tablero[id]) {
+        buscar_camino(tablero, id, selec, visitados, &x, camino, &y, N, M);
+    }
+
+    if (pertenece(camino, N * M, selec)) {
+        for (int i = 0; i < N * M; ++i) {
+            int id_camino = camino[i];
+            if (id_camino != -1) {
+                tablero[id_camino] = 'X';
+            }
+        }
+    }
+
+    free(visitados);
+}
+
+/*
+    tablero: tablero general del juego con casillas marcadas para borrar
+    N y M: dimensiones del tablero
+    dif: dificultad de la partida
+
+    Salida: tablero con casillas borradas, desplazadas, y caramelos nuevos introducidos
+            en caso de que corresponda (mediante puntero a tablero)
+
+*/
+
+__global__ void recolocar_tablero(char* tablero, int N, int M, int* dif) {
     int id = threadIdx.y * N + threadIdx.x;
     int X_debajo = 0; 
     int noX_encima = 0; 
@@ -72,13 +155,10 @@ __device__ void recolocar_tablero(char* tablero, int N, int M) {
         }
     }
 
-    printf("El hilo %d tiene %d no X por encima, y %d X por debajo\n", id, noX_encima, X_debajo); 
-
     __syncthreads();
 
-    if (id + M * X_debajo < N * M && X_debajo > 0) {
+    if (id + M * X_debajo < N * M && X_debajo > 0 && valor_anterior != 'X') {
         tablero[id + M * X_debajo] = valor_anterior; 
-        printf("El hilo %d, escribe en la posicion %d\n", id, id + M * X_debajo); 
     }
     
     if (valor_anterior == 'X') {
@@ -86,7 +166,12 @@ __device__ void recolocar_tablero(char* tablero, int N, int M) {
     }
 
     if (X_debajo - noX_encima > 0) {
-        printf("El hilo %d debe introducir aleatorio\n", id); 
+        if (*dif) {
+            tablero[id] = nuevos_caramelos_dificil[id % 6] + '0'; 
+        }
+        else {
+            tablero[id] = nuevos_caramelos_facil[id % 4] + '0';
+        }
     }
 
 }
@@ -142,42 +227,7 @@ __global__ void bomba_radio(char* tablero, int numero, int N, int M, int fila, i
 }
 */
 
-
-__global__ void encontrar_caminos(char* tablero, int N, int M, int fila, int columna) {
-    int selec = fila * M + columna; 
-    int id = threadIdx.y * N + threadIdx.x; 
-
-    //Funcion que busque camino
-    int* camino = (int*)malloc(N * M * sizeof(int)); 
-    int* visitados = (int*)malloc(N * M * sizeof(int));
-    int x = 1; 
-    int y = 1; 
-
-    for (int i = 1; i < N * M; ++i) {
-        camino[i] = -1; 
-        visitados[i] = -1; 
-    }
-
-    camino[0] = id; 
-    visitados[0] = id; 
-
-    if (tablero[selec] == tablero[id]) {
-        buscar_camino(tablero, id, selec, visitados, &x, camino, &y, N, M);
-    }
-
-    if (pertenece(camino, N * M, selec)) {
-        for (int i = 0; i < N * M; ++i) {
-            int id_camino = camino[i];
-            if (id_camino != -1) {
-                tablero[id_camino] = 'X';
-            }
-        }
-    }
-
-    recolocar_tablero(tablero, N, M); 
-    
-    free(visitados); 
-}
+//Funciones auxiliares (HOST)
 
 void cargar_argumentos(int argc, char* argv[]) {
     if (argc != 5) {
@@ -241,6 +291,7 @@ void mostrar_tablero(char* tablero, int n, int m) {
     }
 }
 
+//Flujo principal
 
 int main(int argc, char* argv[]){
 
@@ -254,12 +305,19 @@ int main(int argc, char* argv[]){
         }
     }
 
+    int* d_dif;
+    cudaMalloc((void**)&d_dif, sizeof(int));
+    cudaMemcpy(d_dif, &dificultad, sizeof(int), cudaMemcpyHostToDevice);
+
     while (vidas > 0) {
         mostrar_tablero(tablero, N, M);
 
         char* d_tablero;
+        
         cudaMalloc((void**)&d_tablero, sizeof(char) * N * M);
+        
         cudaMemcpy(d_tablero, tablero, sizeof(char) * N * M, cudaMemcpyHostToDevice);
+        
 
         int fila;
         int col; 
@@ -268,13 +326,14 @@ int main(int argc, char* argv[]){
         scanf("%d %d", &fila, &col); 
 
         dim3 bloque(N, M);
-        encontrar_caminos <<<1, bloque >>> (d_tablero, N, M, fila, col);
+        encontrar_caminos <<<1, bloque>>> (d_tablero, N, M, fila, col);
+        recolocar_tablero <<<1, bloque>>> (d_tablero, N, M, d_dif); 
         cudaMemcpy(tablero, d_tablero, sizeof(char) * N * M, cudaMemcpyDeviceToHost); 
+        cudaFree(d_tablero);
     }
-
-    
     
     free(tablero); 
+    cudaFree(d_dif); 
 
     return 0;
 }
